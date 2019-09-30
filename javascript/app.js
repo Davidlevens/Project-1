448ab4c02ff34cc1082a440f0746203d9ad8e8a3
David Levens <52017470+Davidlevens@users.noreply.github.com>
2019-09-30 17:53:35 -0400

    Added js function from Anrea to make scroller scroll

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,46 @@
+
+$(function(){
+    var scroller = $('#scroller div.innerScrollArea');
+    var scrollerContent = scroller.children('ul');
+    scrollerContent.children().clone().appendTo(scrollerContent);
+    var curX = 0;
+    scrollerContent.children().each(function(){
+        var $this = $(this);
+        $this.css('left', curX);
+        curX += $this.outerWidth(true);
+    });
+    var fullW = curX / 2;
+    var viewportW = scroller.width();
+
+    // Scrolling speed management
+    var controller = {curSpeed:0, fullSpeed:1};
+    var $controller = $(controller);
+    var tweenToNewSpeed = function(newSpeed, duration)
+    {
+        if (duration === undefined)
+            duration = 1000;
+        $controller.stop(true).animate({curSpeed:newSpeed}, duration);
+    };
+
+    // Pause on hover
+    scroller.hover(function(){
+        tweenToNewSpeed(0);
+    }, function(){
+        tweenToNewSpeed(controller.fullSpeed);
+    });
+
+    // Scrolling management; start the automatical scrolling
+    var doScroll = function()
+    {
+        var curX = scroller.scrollLeft();
+        var newX = curX + controller.curSpeed;
+        if (newX > fullW*2 - viewportW)
+            newX -= fullW;
+        scroller.scrollLeft(newX);
+    };
+    setInterval(doScroll, 30);
+    tweenToNewSpeed(controller.fullSpeed);
+});
 // API MAP
 var map;
 
